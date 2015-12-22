51de612f8f7945465cc783c65e1d966e03a88f7f
Austin Jenchi <timtim17@users.noreply.github.com>
2015-12-22 12:11:36 -0800

    Add mainLoop() and move print() to Table class

--- src/main/java/me/timtim17/dev/java/apcs/textexcel/TextExcel.java
+++ src/main/java/me/timtim17/dev/java/apcs/textexcel/TextExcel.java
@@ -4,10 +4,12 @@ import org.jetbrains.annotations.NotNull;
 import me.timtim17.dev.java.apcs.textexcel.cells.Cell;
 import me.timtim17.dev.java.apcs.textexcel.utility.Utility;
 
+import java.util.Scanner;
+
 public class TextExcel {
     public static void main(String[] args) throws Exception {
         Table table = getNewTable(5, 5);
-        print(table);
+        mainLoop(table);
     }
 
     @NotNull
@@ -15,22 +17,26 @@ public class TextExcel {
         return new Table(rows, cols);
     }
 
-    public static void print(@NotNull Table table) {
-        // TODO: Add a border to the table.
-        // TODO: Add row headers.
-        Cell<?>[][] cells = table.getCells();
-        try {
-            System.out.println(Utility.generateHeaders(cells.length));
-        } catch (IllegalArgumentException e) {
-            e.printStackTrace();
-        }
-        for (Cell<?>[] row : cells) {
-            System.out.print("|");
-            for (Cell<?> cell : row) {
-                System.out.print(cell.getShortenedContents());
-                System.out.print("|");  // TODO: Get better ASCII art
-            }
-            System.out.println();
+    private static void printLogo() {
+        // TODO: ASCII ART
+        System.out.println("TextExcel\n");
+    }
+
+    private static void printCommands() {
+//        System.out.println("");
+    }
+
+    public static void mainLoop(@NotNull Table table) {
+        printLogo();
+        table.print();
+
+        Scanner console = new Scanner(System.in);
+        boolean done = false;
+        while (!done) {
+            System.out.println("Please enter a command:");
+            printCommands();
+            String command = console.nextLine();
+            // TODO: Do Something With This
         }
     }
 }
