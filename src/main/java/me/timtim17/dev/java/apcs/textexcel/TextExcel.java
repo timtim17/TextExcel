c5cb4eda881613b2c4cf573a5ca3b1f4432003d2
Austin Jenchi <timtim17@users.noreply.github.com>
2015-12-27 11:49:24 -0800

    Work on commands

--- src/main/java/me/timtim17/dev/java/apcs/textexcel/TextExcel.java
+++ src/main/java/me/timtim17/dev/java/apcs/textexcel/TextExcel.java
@@ -4,8 +4,7 @@ import org.jetbrains.annotations.NotNull;
 
 import java.util.Scanner;
 
-public class TextExcel {
-    public static void main(String[] args) throws Exception {
+public class TextExcel {public static void main(String[] args) throws Exception {
         Table table = getNewTable(5, 5);
         mainLoop(table);
     }
@@ -21,7 +20,8 @@ public class TextExcel {
     }
 
     private static void printCommands() {
-//        System.out.println("");
+        System.out.println("print\t→\tPrints the table.");
+        System.out.println("quit\t→\tExits the program.");
     }
 
     public static void mainLoop(@NotNull Table table) {
@@ -34,8 +34,20 @@ public class TextExcel {
             System.out.println("Please enter a command:");
             printCommands();
             String command = console.nextLine();
-            // TODO: Do Something With This
+            switch (command) {
+                case "print":
+                    table.print();
+                    break;
+                case "quit":
+                    done = true;
+                    break;
+                default:
+                    System.err.println("INVALID COMMAND");
+                    System.out.println();
+                    break;
+            }
         }
+        System.out.println("Goodbye.");
     }
 
     @Deprecated
