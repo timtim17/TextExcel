dfb957d3460bbf93c2761bf8f1ac3cdc30137f40
Austin Jenchi <timtim17@users.noreply.github.com>
2016-01-22 18:37:26 -0800

    Update jar prefix

--- utility/src/test/java/me/timtim17/dev/java/apcs/textexcel/UtilityTest.java
+++ utility/src/test/java/me/timtim17/dev/java/apcs/textexcel/UtilityTest.java
@@ -7,7 +7,6 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class UtilityTest {
-
   @Test
   public void testGenerateHeaders() {
     assertEquals("|A       |", Utility.generateHeaders(1));
